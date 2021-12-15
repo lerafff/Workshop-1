cde62d1d9eb6299506e860d0674f063d1f42ecdd
fazhur <fedir.zhurba@ucu.edu.ua>
2021-12-15 12:07:55 +0200

    Exit + 10 bug fix

--- workshop.py
+++ workshop.py
@@ -2,10 +2,16 @@ def input_read():
     while True:
         print('Введіть координату клітинки:')
         point = input()
-        if len(point) != 2 or not point[0].isalpha or not point[1].isnumeric:
+        if point == 'exit':
+            print('Дякую за гру!')
+            exit()
+        elif point[1:] == '10' and len(point) == 3 and point[0].isalpha:
+            return (ord(point[0]) - 97, int(point[1:]) - 1)
+        elif len(point) != 2 or not point[0].isalpha or not point[1].isnumeric:
             print('Некоректна координата!')
-        elif point[0] not in 'abcdefghij' or point[1] not in range(1,11):
+        elif point[0] not in 'abcdefghij' or int(point[1]) not in range(1,11):
             print('Координата виходить за межі поля')
         else:
             break
-    return (ord(point[0]) - 97, point[1] - 1)
+    return (ord(point[0]) - 97, int(point[1]) - 1)
+
