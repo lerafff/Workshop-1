0de9112b68edab1902d524902a167e18d6da43f6
Taras <taras.yaroshko@ucu.edu.ua>
2021-12-15 12:13:42 +0200

    Removed comments

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ from typing import List
 
 
 def printMap(map: List[List[int]]):
-    # print("    A B C D E F H I G H")
-    # row_number = 1
-    # for row in map:
-    #     mid_str = "■".join(row)
-    #     # if row_number != 10:
-    #     #     print(f"{row_number}  |{mid_str}|")
-    #     # else:
-    #     print(f"{row_number} |{mid_str}|")
-    #     row_number = row_number + 1
     print("    A B C D E F G H I J")
     row_number = 1
     for row in map:
@@ -21,17 +12,3 @@ def printMap(map: List[List[int]]):
             mid = "■".join(row) + "■ "
             print(f"{row_number} |{mid}|")
         row_number = row_number + 1
-
-
-printMap([
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-])
