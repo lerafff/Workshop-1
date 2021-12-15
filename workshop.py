f13cb688bc094e575e319df4d497ccf6605eba65
fazhur <fedir.zhurba@ucu.edu.ua>
2021-12-15 12:29:10 +0200

    game flow started

--- workshop.py
+++ workshop.py
@@ -15,3 +15,26 @@ def input_read():
             break
     return (ord(point[0]) - 97, int(point[1]) - 1)
 
+
+def ships_destroyed(map):
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 2:
+                return False
+    return True
+
+
+def game():
+    print('Хід першого гравця:')
+    map1 = read_input()
+    print('Хід другого гравця:')
+    map2 = read_input()
+    while not ships_destroyed(map1) and not ships_destroyed(map2):
+        print('Хід першого гравця:')
+        print(shots_map2)
+        shot = input_read()
+        print(shots_map1)
+        print('Хід другого гравця:')
+        shot = input_read()
+
+
